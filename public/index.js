5699e6b4dc243f15ff144df7f0c2422d6ec91fff
Justin Campbell <campbjus@flip1.engr.oregonstate.edu>
2018-06-12 15:43:17 -0700

    index.js

--- public/index.js
+++ public/index.js
@@ -1,32 +1,32 @@
-function getPersonName(){
-
-
-}
-
-function getNumberOfWins(){
-
-
-}
-
-function getTotalGames(){
-
-
-
-} 
-
 function storePerson(personName, numberOfWins, totalGames){
 	var request= new XMLHttpRequest();
 
-	var personName=getPersonName();
-	var numberOfWins=getNumberOfWins();
-	var totalGames=getTotalGames();
 	
 
-	var requestURL= personName + numberOfWins + totalGames;
+	var requestURL= "/data";
 	request.open("POST", requestURL);
 	
-	//var requestBody=JSON.stringify({
+	var requestBody=JSON.stringify({
+			
+	)};
+
+	request.addEventListener('load', function(event){
+		if(event.target.status===200){
+			
+
+
+		}
+		else{
+			alert("Error storing information");
+
+		}
+
+
+
+	}
 	
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.send(requestBody);
 
 }
 
