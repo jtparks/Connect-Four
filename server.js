31dcec997fe3dc1f9e35c72b5da9728337be30e2
Arnav Bhutani <bhutania@oregonstate.edu>
2018-06-12 20:06:59 -0700

    got server to work properly with mongo

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ var bodyParser = require('body-parser');
 var MongoClient=require('mongodb').MongoClient;
 var app = express();
 
-var mongoHost=process.env.MONGO_HOST;
-var mongoPort= process.env.MONGO_PORT || '37691' ;
-var mongoUsername='Bhutania';
-var mongoPassword='asdfgh1';
-var mongoDBName=process.env.MONGO_DB_NAME;
+var mongoHost='classmongo.engr.oregonstate.edu';
+var mongoPort= process.env.MONGO_PORT || '27017' ;
+var mongoUser='cs290_bhutania';
+var mongoPassword='cs290_bhutania';
+var mongoDBName='cs290_bhutania';
 
-//var mongoURL = "mongo ds036079.mlab.com:36079/thecorrectteam -u bhutania -p asdfgh1"
-
-var mongoURL="mongodb://" + mongoUsername + ":" + mongoPassword + "@" + "ds036079.mlab.com:36079/thecorrectteam";
+var mongoURL='mongodb://' + mongoUser + ':' + mongoPassword + '@' +
+mongoHost + ':' + mongoPort + '/' + mongoDBName;
 
 var mongoDB=null;
 
