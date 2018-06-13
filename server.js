2992cfe33776a80becf7d20b1908a20b4604ede4
Arnav Bhutani <bhutania@oregonstate.edu>
2018-06-12 18:53:14 -0700

    installed mongodb

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('*', function(req, res){
   res.render('404');
 });
 
-MongoCLient.connect(mongoURL, function(err, client){
+MongoClient.connect(mongoURL, function(err, client){
 	if(err){
 		throw err;
 	}
